f918fa75c3b65bb798472c0f00e58fff6d313199
Phellipe <phellipess_17@hotmail.com>
2023-11-01 15:43:46 -0300

    refatoração e formatação

--- daemon_check_driver.py
+++ daemon_check_driver.py
@@ -20,9 +20,9 @@ def imu_check():
     command2 = 'i2cdetect -y 1 | grep -ia 68'
     result, error=run_bash_command(command2)
     if (result != ''):
-        return 'On'
+        return '\033[1;32;40m ON \033[0m'
     else:
-        return 'Off'
+        return '\033[1;31;40m Off \033[0m'
     # print(result)
     # ---- Opcao 2 -----
     # bus_number = 1
@@ -41,15 +41,12 @@ def imu_check():
 # It is designed to terminate the process after reading the first line.
 def capture_first_line(command):
     first_line=''
-    d=0
     try:
         process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
         lines= process.stdout.readlines()
         process.terminate()  # Encerra o processo após ler a primeira linha
         if lines:
-            for i in range(3):
-                first_line+= lines[d].strip()
-                d+=1
+            first_line= lines[0].strip()
             return first_line
         else:
             return ''
@@ -63,15 +60,13 @@ def check_internet():
     try:
         # Tente fazer uma conexão com um servidor remoto (por exemplo, o Google)
         socket.create_connection(("www.google.com", 80))
-        return True
+        return '\033[1;32;40m Online \033[0m'
     except OSError:
-        return False
+        return '\033[1;31;40m Offline \033[0m'
 
 # get_machine_storage(): This function calculates and returns the total and free storage space on the root filesystem. 
 # If the total storage is less than 10 GB, it attempts to expand the root filesystem and requests a reboot.
 def get_machine_storage():
-    command_expand = 'sudo raspi-config --expand-rootfs'
-    reboot = 'sudo .driver_analytics/ask_for_reboot.sh'
     phrase = ''
     result=os.statvfs('/')
     block_size=result.f_frsize
@@ -84,14 +79,14 @@ def get_machine_storage():
     total_size = round(total_size)
     free_size = round(free_size)
     if (total_size > 10):
-        phrase = ' expandido'
+        phrase = '\033[1;32;40m expandido\033[0m'
     else:
-        phrase = ' nao expandido'
+        phrase = '\033[1;31;40m nao expandido\033[0m'
 
     if (free_size < 0.1 * total_size):
-        phrase += ' e com problema'
+        phrase += ' e \033[1;31;40mcom excesso de arquivos \033[0m'
     else:
-        phrase += ' e saudavel'
+        phrase += ' e \033[1;32;40msem excesso de arquivos\033[0m'
     return phrase
 
 # clear_log_file(log_file_path): This function clears the contents of a log file specified by log_file_path.
@@ -110,25 +105,26 @@ def chk_gps():
     linha1 = linha1[:10]
     #print(f' info gps: {linha1}')
     if (linha1 == '$GNGSA,A,3' or linha1 == '$GNGSA,A,2'):
-        return 'GPS ON'
+        return 'GPS\033[1;32;40m ON \033[0m'
     else:
-        return 'GPS OFF'
+        return ' GPS\033[1;31;40m OFF \033[0m'
     
-#Cheking gps health
+#Cheking gps health with bytes
 def chk_gps2():
     gps_device_fd = "/dev/serial0"
     gps_device = os.open(gps_device_fd, os.O_RDWR)
     gps_data = ""
     for i in range(1024):
-        gps_data += os.read(gps_device, 2048)   
+        #gps_data += os.read(gps_device, 2048)
+        gps_data += os.read(gps_device, 2048).decode('utf-8')  
     if "$GNGSA,A,3" in gps_data or "$GNGSA,A,2" in gps_data:
         #print("GPS: OK");    
         #print("\033[1;32;40m GPS: OK \033[0;37;40m");
-        return "\033[1;32;40m GPS: OK \033[0;37;40m"
+        return "GPS:\033[1;32;40mOK \033[0m"
     else:
         #print("GPS: ERROR");
         #print("\033[1;31;40m GPS: ERROR \033[0;37;40m");
-        return "\033[1;31;40m GPS: ERROR \033[0;37;40m"
+        return "GPS:\033[1;31;40mERROR \033[0m"
 
 # chk_camera(): This function checks if a process named "camera" is running. If the process is running, 
 # it returns "Camera On," otherwise "Camera OFF."
@@ -136,9 +132,9 @@ def chk_camera():
     camera_command = 'pgrep camera'
     result, error=run_bash_command(camera_command)
     if(result != ''):
-        return 'Camera On'
+        return 'Camera:\033[1;32;40m ON \033[0m'
     else:
-        return 'Camera OFF'
+        return 'Camera:\033[1;31;40m OFF \033[0m'
 
 """
 The main part of the script starts here.
@@ -156,23 +152,15 @@ def main():
     clear_log_file(log_file_path)  # Apaga o conteúdo do arquivo de log ao iniciar
     while True:
         with open(f'/tmp/{daemon_name}.log', 'a') as file:
-            current_time = time.strftime('%Y-%m-%d %H:%M:%S')
-            #gps_command = 'cat /dev/serial0 | grep -ia gsa'
-            #linha1 = capture_first_line(gps_command)
-            #partition = '/dev/root'
-            #partition = '/boot/efi'
+            current_time = time.strftime('\033[1;36;40m%Y-%m-%d %H:%M:%S\033[0m')
             size = get_machine_storage()
-            status_gps=chk_gps()
+            status_gps=chk_gps2()
             status_camera=chk_camera()
-            if check_internet():
-                conncetion_chk =  " Online"
-            else:
-                conncetion_chk = "Offline"
+            conncetion_chk = check_internet()
             imu = imu_check()
             file.write(f'{current_time} - Status conexao: {conncetion_chk} - Sd card: {size}  - {status_gps} - {status_camera} - IMU: {imu}\n')
-            print('Gerando log...')
+            print('\033[1;32;40m Gerando log...\033[0m')
             imu_check()
-
         time.sleep(3)
 
 
